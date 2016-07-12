646a3fd9d8b6ecfce5834edfb4c3051bc9a1909f
Alexaner Kozlov <a.kozlov.box@yandex.ru>
2016-07-12 11:16:15 +0300

    modifyed timestamping

--- l3-multi-destinations-latency.lua
+++ l3-multi-destinations-latency.lua
@@ -9,6 +9,7 @@ local stats		= require "stats"
 
 local SRC_IP = parseIPAddress("192.168.1.2")
 local DST_IP = {parseIPAddress("10.0.0.1"), parseIPAddress("10.255.255.255")}
+local DST_ETH = "90:e2:ba:3f:c7:00"
 
 local PKT_SIZE = 60
 
@@ -46,7 +47,7 @@ local mem = memory.createMemPool(function(buf)
 		buf:getUdpPacket():fill{
 			pktLength = PKT_SIZE,
 			ethSrc = txQueue,
-			ethDst = "90:e2:ba:3f:c7:00",
+			ethDst = DST_ETH,
 			ip4Src = SRC_IP,
 			udpSrc = 1234,
 			udpDst = 5678,	
@@ -79,8 +80,19 @@ end
 function runTest(txQueue, rxQueue)
 	local timestamper = ts:newTimestamper(txQueue, rxQueue)
 	local hist = hist:new()
+	if size < 84 then
+		log:warn("Packet size %d is smaller than minimum timestamp size 84. Timestamped packets will be larger than load packets.", size)
+		size = 84
+	end
+	dpdk.sleepMillis(1000)
 	while dpdk.running() do
-		hist:update(timestamper:measureLatency())
+		hist:update(timestamper:measureLatency(size, function(buf)
+			fillUdpPacket(buf, size)
+			local pkt = buf:getUdpPacket()
+			pkt.ip4.src:set(SRC_IP)
+			pkt.ip4.dst:set(math.random(DST_IP[1], DST_IP[2]))
+			pkt.eth.dst:set(DST_ETH)
+		end))
 	end
 	hist:save("histogram.csv")
 	hist:print()
